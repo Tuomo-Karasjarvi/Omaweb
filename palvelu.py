f22cc9ec2af3221e26ae670e14a5d61de0e20268
Tuomo Karasjärvi <Tuomo.Karasjarvi@edu.mayk.fi>
2025-11-01 11:49:31 +0200

    commit palvelu.py

--- palvelu.py
+++ palvelu.py
@@ -8,8 +8,11 @@ Created on Sat Nov  1 09:23:02 2025
 from wsgiref.simple_server import make_server
 
 def app(environ, respond):
-	respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-	yield "Sigma grindset!".encode('utf-8')
+    respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+    nimi = environ['PATH_INFO'].strip('/')
+    salanimi = nimi.replace('a', 'apa').replace('i', 'ipi') \
+        .replace('n', 'non').replace('na', 'nana')
+    yield ("salainen nimesi on: %s\n" % salanimi).encode('utf-8')
 
 if __name__ == '__main__':
 	with make_server("localhost", 8001, app) as server: 
